824aadf80265945c9b36857fd7496f6d2aab5aff
ttphan06 <thanphan96@gmail.com>
2018-11-21 14:42:14 -0800

    change to test program of TA

--- stack.c
+++ stack.c
@@ -1,6 +1,37 @@
 #include "types.h"
 #include "user.h"
 
+// Prevent this function from being optimized, which might give it closed form
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+static int
+recurse(int n)
+{
+  if(n == 0)
+    return 0;
+  return n + recurse(n - 1);
+}
+#pragma GCC pop_options
+
+int
+main(int argc, char *argv[])
+{
+  int n, m;
+
+  if(argc != 2){
+    printf(1, "Usage: %s levels\n", argv[0]);
+    exit();
+  }
+
+  n = atoi(argv[1]);
+  printf(1, "Lab 3: Recursing %d levels\n", n);
+  m = recurse(n);
+  printf(1, "Lab 3: Yielded a value of %d\n", m);
+  exit();
+}
+
+
+/*
 long long fib(long long n){
   if (n == 0)
     return 1;
@@ -17,7 +48,9 @@ int nest(int n){
 }
 int main(int argc, char *argv[])
 {
-  
+  //  int *i = malloc(2147459068 * sizeof(int));
+  //  i[1] = 1;
+
   long long n = 0;
   if (argc > 1)
     n = atoi(argv[1]);
@@ -26,4 +59,5 @@ int main(int argc, char *argv[])
   nest(n);
   exit();
 }
+*/
 	

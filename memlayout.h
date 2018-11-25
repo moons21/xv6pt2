695cc6af5ee9b44ca241fb40098097e37d186fe4
andrew munoz <amuno032@sledge.cs.ucr.edu>
2018-11-25 15:25:03 -0800

    Found areas in code which shows why the stack never runs into the heap

--- memlayout.h
+++ memlayout.h
@@ -1,7 +1,11 @@
 // Memory layout
 
 #define EXTMEM  0x100000            // Start of extended memory
-#define PHYSTOP 0xE000000           // Top physical memory
+
+/* This parameters limits xv6 to how much ram can be used */
+#define PHYSTOP 0xE000000           // Top physical memory // 232 bytes
+//#define PHYSTOP 0x77359400          // Top physical memory // 2Gb 
+
 #define DEVSPACE 0xFE000000         // Other devices are at high addresses
 
 // Key addresses for address space layout (see kmap in vm.c for layout)
